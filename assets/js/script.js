cbc558b3d55988a008478ec158cbf18e06a4ba3a
Carolane LEFEBVRE <107761189+CarolaneLFBV@users.noreply.github.com>
2022-12-03 11:50:53 +0100

    ✨ Media Screen added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 const btnMenu = document.getElementById("btnMenu");
 const navList = document.getElementById("navList");
 const content = document.getElementById("content");
+const footer  = document.getElementById("footer");
 
 btnMenu.addEventListener("click", e => {
     if(navList.style.display !== "none") {
         navList.style.display = "none"; 
-        content.style.display = "block"; 
+        content.style.display = "block";
+        footer.style.display="block"; 
     } 
     else { 
         navList.style.display = "block";
         content.style.display = "none"; 
+        footer.style.display = "none"; 
     }
 })
