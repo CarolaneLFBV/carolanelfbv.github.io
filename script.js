107a083ef65576099d16f540f7744dc2d5cc7c50
CarolaneLFBV <107761189+CarolaneLFBV@users.noreply.github.com>
2023-11-04 17:59:56 +0100

    feat: skills popup

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggle() {
+    var content = document.getElementById('content-blur');
+    content.classList.toggle('active');
+
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
