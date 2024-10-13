49dd8047c545dbbb846cd90e0fff880c8f92bda5
CarolaneLFBV <carolane_lefebvre@icloud.com>
2024-10-13 20:14:35 +0200

    BREAKING CHANGE: WIP

--- script.js
+++
@@ -1,7 +0,0 @@
-function toggle() {
-    var content = document.getElementById('content-blur');
-    content.classList.toggle('active');
-
-    var popup = document.getElementById('popup');
-    popup.classList.toggle('active');
-}
